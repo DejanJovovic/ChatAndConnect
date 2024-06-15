7b017d1817adc89907947930ccb5fe86936557b9
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2024-06-15 20:01:15 +0200

    displaying messages sent to Firestore on chat_messages screen with StreamBuilder

--- chat_app/lib/widgets/chat_messages.dart
+++ chat_app/lib/widgets/chat_messages.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -5,8 +6,44 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No messages found.'),
+    //listen to a stream of messages, when a new message is submitted its displayed here
+    return StreamBuilder(
+      // this sets up a listener that listen to a chat collection, when a new chat is added it will trigger the builder below
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy(
+            'createdAt',
+            descending: false,
+          )
+          .snapshots(),
+      builder: (ctx, chatSnapShots) {
+        if (chatSnapShots.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!chatSnapShots.hasData || chatSnapShots.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No messages found.'),
+          );
+        }
+
+        if (chatSnapShots.hasError) {
+          return const Center(
+            child: Text('Something went wrong...'),
+          );
+        }
+
+        final loadedMessages = chatSnapShots.data!.docs;
+
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (ctx, index) => Text(
+            loadedMessages[index].data()['text'],
+          ),
+        );
+      },
     );
   }
 }
