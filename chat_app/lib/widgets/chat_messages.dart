60385e15f14e7f77a4b40b471b5dd3ec752fa87b
Dejan Jovovic <dejan.jovovic2002@gmail.com>
2024-06-15 21:15:35 +0200

    username and image is displayed for the message of the user who sent it

--- chat_app/lib/widgets/chat_messages.dart
+++ chat_app/lib/widgets/chat_messages.dart
@@ -1,4 +1,6 @@
+import 'package:chat_app/widgets/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -6,6 +8,8 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authenticatedUser = FirebaseAuth.instance.currentUser!;
+
     //listen to a stream of messages, when a new message is submitted its displayed here
     return StreamBuilder(
       // this sets up a listener that listen to a chat collection, when a new chat is added it will trigger the builder below
@@ -13,7 +17,7 @@ class ChatMessages extends StatelessWidget {
           .collection('chat')
           .orderBy(
             'createdAt',
-            descending: false,
+            descending: true,
           )
           .snapshots(),
       builder: (ctx, chatSnapShots) {
@@ -38,10 +42,39 @@ class ChatMessages extends StatelessWidget {
         final loadedMessages = chatSnapShots.data!.docs;
 
         return ListView.builder(
-          itemCount: loadedMessages.length,
-          itemBuilder: (ctx, index) => Text(
-            loadedMessages[index].data()['text'],
+          padding: const EdgeInsets.only(
+            bottom: 40,
+            left: 13,
+            right: 13,
           ),
+          // pushes messages to the bottom of this ListView
+          reverse: true,
+          itemCount: loadedMessages.length,
+          itemBuilder: (ctx, index) {
+            final chatMessage = loadedMessages[index].data();
+            final nextChatMessage = index + 1 < loadedMessages.length
+                ? loadedMessages[index + 1].data()
+                : null;
+
+            final currentMessageUserId = chatMessage['userId'];
+            final nextMessageUserId =
+                nextChatMessage != null ? nextChatMessage['userId'] : null;
+            final nextUserIsSame = nextMessageUserId == currentMessageUserId;
+
+            if (nextUserIsSame) {
+              return MessageBubble.next(
+                message: chatMessage['text'],
+                isMe: authenticatedUser.uid == currentMessageUserId,
+              );
+            } else {
+              return MessageBubble.first(
+                userImage: chatMessage['userImage'],
+                username: chatMessage['username'],
+                message: chatMessage['text'],
+                isMe: authenticatedUser.uid == currentMessageUserId,
+              );
+            }
+          },
         );
       },
     );
---
+++ chat_app/lib/widgets/message_bubble.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+
+// a MessageBubble for showing a single chat message on the ChatScreen
+class MessageBubble extends StatelessWidget {
+
+  // create a message bubble which is meant to be the first in the sequence
+  const MessageBubble.first({
+    super.key,
+    required this.userImage,
+    required this.username,
+    required this.message,
+    required this.isMe,
+  }) : isFirstInSequence = true;
+
+  // create a amessage bubble that continues the sequence
+  const MessageBubble.next({
+    super.key,
+    required this.message,
+    required this.isMe,
+  })  : isFirstInSequence = false,
+        userImage = null,
+        username = null;
+
+  // whether or not this message bubble is the first in a sequence of messages from the same user
+  // modifies the message bubble slightly for these different cases - only
+  // shows user image for the first message from the same user, and changes
+  // the shape of the bubble for messages thereafter
+  final bool isFirstInSequence;
+
+  // not required if the message is not the first in a sequence
+  final String? userImage;
+
+  // Not required if the message is not the first in a sequence.
+  final String? username;
+  final String message;
+
+  // Controls how the MessageBubble will be aligned.
+  final bool isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Stack(
+      children: [
+        if (userImage != null)
+          Positioned(
+            top: 15,
+            // Align user image to the right, if the message is from me
+            right: isMe ? 0 : null,
+            child: CircleAvatar(
+              backgroundImage: NetworkImage(
+                userImage!,
+              ),
+              backgroundColor: theme.colorScheme.primary.withAlpha(180),
+              radius: 23,
+            ),
+          ),
+        Container(
+          margin: const EdgeInsets.symmetric(horizontal: 46),
+          child: Row(
+            mainAxisAlignment:
+                isMe ? MainAxisAlignment.end : MainAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment:
+                    isMe ? CrossAxisAlignment.end : CrossAxisAlignment.start,
+                children: [
+                  // first messages in the sequence provide a visual buffer at the top.
+                  if (isFirstInSequence) const SizedBox(height: 18),
+                  if (username != null)
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 13,
+                        right: 13,
+                      ),
+                      child: Text(
+                        username!,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black87,
+                        ),
+                      ),
+                    ),
+
+                  // the "speech" box surrounding the message
+                  Container(
+                    decoration: BoxDecoration(
+                      color: isMe
+                          ? Colors.grey[300]
+                          : theme.colorScheme.secondary.withAlpha(200),
+                      // only show the message bubble's "speaking edge" if first is the chain
+                      // whether the "speaking edge" is on the left or right depends on whether or not the message bubble is the current user
+                      borderRadius: BorderRadius.only(
+                        topLeft: !isMe && isFirstInSequence
+                            ? Radius.zero
+                            : const Radius.circular(12),
+                        topRight: isMe && isFirstInSequence
+                            ? Radius.zero
+                            : const Radius.circular(12),
+                        bottomLeft: const Radius.circular(12),
+                        bottomRight: const Radius.circular(12),
+                      ),
+                    ),
+                    constraints: const BoxConstraints(maxWidth: 200),
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 14,
+                    ),
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 4,
+                      horizontal: 12,
+                    ),
+                    child: Text(
+                      message,
+                      style: TextStyle(
+                        height: 1.3,
+                        color: isMe
+                            ? Colors.black87
+                            : theme.colorScheme.onSecondary,
+                      ),
+                      softWrap: true,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
